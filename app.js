3a64318e318e6971417178b88c615c6186d6be24
Le Vu Nhat Truong <nhattruong0210@hotmail.com>
2019-10-21 15:50:11 +0700

    Update information

--- app.js
+++ app.js
@@ -47,93 +47,21 @@ const messages = {
       title: 'Our work',
       body: 'We\'re passionate about creating beautiful work for our clients. Below you can see some examples of our creations. Contact us today for a quote.',
       website: 'Website',
-      beautizon: {
-        title: 'Beautizon',
-        bodyHTML: '<p>Beautizon is a Vietnamese company based in Ho Chi Minh City that imports high-quality skincare products from Australia and New Zealand.</p> <p>24h.dev completely rebranded Beautizon, including the logo and brand guidelines. We then continued to build a world-class website with an admin area that allowed the client to customise the website as they saw fit.</p>'
+      clascity: {
+        title: 'Clascity',
+        bodyHTML: '<p>Search and connect with tutors from across the web. In just a few clicks, you will be learning from the best teachers and gurus out there.</p>'
       },
-      boobytrapp: {
-        title: 'Boobytrapp',
-        bodyHTML: '<p>Boobytrapp is a mobile app that connects breast cancer patients globally and eases the burden of treatment management through innovative features.</p> <p>24h.dev wireframed, designed and built the prototype, website and mobile app (Android and iOS) for Boobytrapp.</p>'
-      },
-      oliviasPrime: {
-        title: 'Olivia\'s Prime Restaurant',
-        bodyHTML: '<p>Without a shadow of doubt Olivia\'s is the best steak in Vietnam. Imported Wagyu tomahawk steaks cooked over a woodfire - even better than it sounds! We designed and developed a site for Olivia\'s that would exude flavour. We think we did alright...</p>'
-      },
-      phoenix: {
-        title: 'Phoenix Consult',
-        bodyHTML: '<p>Phoenix Consult is a Singaporean tech company that specialises in app development and startup consultation services.</p> <p>24h.dev was hired to design the logo, create a brand identity and build a website on a tight budget.'
-      },
-      himalayan: {
-        title: 'Himalayan Salt Co.',
-        bodyHTML: '<p>Himalayan is a UK-based company that sells Himalayan salt blocks which you can cook on.</p> <p>24h.dev designed the logo, branding, packaging and website for Himalayan and advised on a growth strategy.'
-      },
-      secretGarden: {
-          title: 'The Secret Garden',
-          bodyHTML: '<p>The Secret Garden is a hip and trendy bar, cafe and hostel based in the heart of downtown Da Nang. Taking design inspiration from Japanese minimalism, the rooms are decked out with futons and the abundance of concrete gives off a cool air, literally!</p>'
-      },
-      anBang: {
-        title: 'An Bang Beachside Resort',
-        bodyHTML: '<p>An Bang Beachside Boutique Resort is a luxury hotel based on the Central coast of Vietnam.</p> <p>24h.dev designed and developed the Beachside website and integrated their booking system to the site.</p>'
-      }
     },
     about: {
       title: 'About',
-      body: 'Founded in 2018, 24h.dev is run by entrepreneurs with proven track records at C-level positions in successful tech startups. Based in Da Nang and London, with a growing team of over 15 developers, we’re delighted to be your guide from that Eureka moment to the reality of your new product. Meet just a few of the many key members of the team below:',
-      will: {
-        role: 'Founder, CEO',
-        bio: 'Co-founder of Boobytrapp and formerly COO at Rota, a high-growth London-based startup, Will has over ten years\' hospitality and tech experience, working with the likes of onefinestay, The Four Seasons, Itsu and The Capital Hotel before leaving London behind for Vietnam. A wannabe foodie, Will tried his hand as a chef before ultimately realising that no - one trusts a skinny cook.'
-      },
-      cj:  {
-         role: 'Partner, London',
-         bio: 'Our overseas Kiwi, CJ, is co-founder of Planes Studio - a London-based development company who collaborate with 24h.dev Tech. CJ was a pilot in the NZ airforce though never got to realise his ultimate dream of being Top Gun as he headed over to London, becoming a tech specialist at Rota where he met Will.'
-      },
-      sao: {
-        role: 'Head of Design',
-        bio: 'Winner of the best logo design in Vietnam, Sao has worked with global brands, including Air Asia, designing websites and apps. A true artist, Sao has his own personal logo tattooed onto his arm, embroided on his wallet and even stamped on the back of his bike helmet! Sao loves steak and Arsenal FC.'
-      },
+      body: 'Founded in 2018, 24h.dev is run by entrepreneurs with proven track records at C-level positions in successful tech startups. Based in Da Nang, with a growing team of over 15 developers, we’re delighted to be your guide from that Eureka moment to the reality of your new product. Meet just a few of the many key members of the team below:',
       vien: {
         role: 'Head of Development',
         bio: 'Vien runs our ever-growing team of 15 developers across two offices in Da Nang and has over 10 years of web and app development and software engineer management to his name. He thoroughly enjoys interacting with clients and ensuring they\'re nothing but delighted with the outcome of their projects.'
       },
-      nghi: {
-        role: 'Project Manager',
-        bio: 'Vietnamese born and bred, Nghi grew up in Da Nang but spent 4-5 years living in Dazaifu, Japan. A self-confessed lover of memes and general chatterbox, Nghi brings the Japanese work ethic to Vietnam making him an indispensable member of the 24h.dev team. In his spare time he makes robots.'
-      },
-      joshua: {
-        role: 'Account Manager',
-        bio: 'Josh is Denver born and studied design, mathematics, and English while in college. He then moved on to the Army and spent a year in Iraq. After that spent 12 years in the mobile TV engineering business. He enjoys being awkward and attempting to at least receiving a pity laugh out of someone...literally, anyone. He\'s not picky.'
-      },
-      craig: {
-        role: 'Blockchain Advisor',
-        bio: 'Hailing from the Gold Coast in Australia, Craig brings an abundance of knowledge advising 24h.dev Tech on blockchain projects. Craig has a degree in Environmental Management & Sustainability Science and combines this with his tech experience to create purposeful community projects, using blockchain technology for benevolent purposes.'
-      },
-      duyen: {
-        role: 'Operations Manager',
-        bio: 'Duyen gained experience working for CocCoc (a Vietnamese web browser) and exudes all the passion of Vietnamese negotiation, without the aggression! Duyen was a national table tennis champion before joining the tech world.'
-      },
       nhattruong: {
         role: 'CTO',
         bio: 'Truong boasts 8 years of experience in the IT industry and is a security expert, project manager extrordinaire and pretty good badminton player.'
-      },
-      thuy: {
-        role: 'Sales Executive',
-        bio: 'A recent English language grad from Da Nang Foreign Language University, Thuy is not your average Sales rep. Part time model and regular blood donor, Thuy\'s empathetic approach to the Sales process is a breath of fresh air for 24h.dev\'s partners and clients alike.'
-      },
-      nhung: {
-        role: 'Graphic Designer',
-        bio: 'Artist at home, artist at work - you\'ll find Nhung designing or thinking about design wherever she goes. For a quiet and gentle soul, it always surprises us to hear pounding techno music coming from Nhung\'s headphones - whatever floats your boat!'
-      },
-      stella: {
-        role: 'COO',
-        bio: 'Stella is a goal oriented operations maestro with experience at Next and vChain. She relocated her whole family from Sweden in order to join the 24h.dev team, for her to be a part of our success story and to enjoy the lovely Vietnamese weather and cuisine. Born and raised in Sweden Stella drinks coffee like no one else saying that its the secret to her winning concept of how to shine in business.'
-      },
-      bao: {
-        role: 'Sales & Marketing Executive',
-        bio: 'Bao claims he looks like a 30-year-old veteran, but he has the soul of a teenager: energy is his horse and ideas are his weapons. Digital or business warfare, Bao always jumps in with pride. Bao loves working and ensures he looks after our clients with passion, care and empathy.'
-      },
-      dory: {
-        role: 'Operations Executive',
-        bio: 'Sprightly & confident on the outside but caring & warm-hearted on the inside, Dory is always bringing vast amounts of energy to her surroundings. A language enthusiast, Dory speaks and writes impeccable English and is constantly learning new phrases, occasionally contributing to our curse jar along the way.'
       },
       vudang: {
         role: 'Developer',
@@ -252,62 +180,19 @@ new Vue({
       selectedJob: 0,
       jobs: [
         {
-          title: this.$t('work.beautizon.title'),
-          description: this.$t('work.beautizon.bodyHTML'),
-          link: 'http://beautizon.com.vn',
-          image: 'Beautizon.png'
+          title: this.$t('work.clascity.title'),
+          description: this.$t('work.clascity.bodyHTML'),
+          link: 'https://clascity.com',
+          image: 'clascity.png'
         },
-        {
-          title: this.$t('work.boobytrapp.title'),
-          description: this.$t('work.boobytrapp.bodyHTML'),
-          link: 'https://www.boobytrapp.io/',
-          image: 'boobytrapp.jpg'
-        },
-        {
-          title: this.$t('work.secretGarden.title'),
-          description: this.$t('work.secretGarden.bodyHTML'),
-          link: 'https://www.thesecretgarden.vn',
-          image: 'secret-garden.png'
-        },
-        {
-          title: this.$t('work.phoenix.title'),
-          description: this.$t('work.phoenix.bodyHTML'),
-          link: 'https://www.phoenixconsultltd.com',
-          image: 'Phoenix.png',
-        },
-        {
-          title: this.$t('work.oliviasPrime.title'),
-          description: this.$t('work.oliviasPrime.bodyHTML'),
-          link: 'https://oliviasprime.com',
-          image: 'olivias.png',
-        },
-        {
-          title: this.$t('work.himalayan.title'),
-          description: this.$t('work.himalayan.bodyHTML'),
-          link: 'https://www.himalayan.biz',
-          image: 'Himalayan.png',
-        },
-        {
-          title: this.$t('work.anBang.title'),
-          description: this.$t('work.anBang.bodyHTML'),
-          link: 'https://anbangbeachside.com',
-          image: 'beachside.png',
-        }
       ],
       team: [
         {
           name: 'Luong Vien',
-          role: this.$t('about.will.role'),
-          bio: this.$t('about.will.bio'),
-          image: 'will@2x.jpg',
-          linkedIn: 'https://www.linkedin.com/in/will-macfarlane-b289a333/'
-        },
-        {
-          name: 'Stella Sevegran',
-          role: this.$t('about.stella.role'),
-          bio: this.$t('about.stella.bio'),
-          image: 'Stella.png',
-          linkedIn: 'https://www.linkedin.com/in/stella-sevegran-61b6a4a5/'
+          role: this.$t('about.vien.role'),
+          bio: this.$t('about.vien.bio'),
+          image: 'vien_avt.png',
+          linkedIn: ''
         },
         {
           name: 'Nhat Truong',
@@ -317,104 +202,76 @@ new Vue({
           linkedIn: 'https://www.linkedin.com/in/truongtroy/'
         },
         {
-          name: 'Luong Vien',
-          role: this.$t('about.vien.role'),
-          bio: this.$t('about.vien.bio'),
-          image: 'vien_avt.png',
-          linkedIn: 'https://www.linkedin.com/in/will-macfarlane-b289a333/'
-        },
-        {
           name: 'Vu Dang',
           role: this.$t('about.vudang.role'),
           bio: this.$t('about.vudang.bio'),
           image: 'vudang_avt.jpg',
-          linkedIn: 'https://www.linkedin.com/in/will-macfarlane-b289a333/'
-        },
-        {
-          name: 'Sao Nguyen',
-          role: this.$t('about.sao.role'),
-          bio: this.$t('about.sao.bio'),
-          image: 'Sao.jpg',
-          linkedIn: 'https://www.linkedin.com/in/sao-n-0591b293/'
-        },
-        {
-          name: 'Duyen Huynh',
-          role: this.$t('about.duyen.role'),
-          bio: this.$t('about.duyen.bio'),
-          image: 'Duyen.jpg',
-          linkedIn: 'https://www.linkedin.com/in/duyen-huynh-442633120/'
-        },
-        {
-          name: 'Joshua Winter',
-          role: this.$t('about.joshua.role'),
-          bio: this.$t('about.joshua.bio'),
-          image: 'josh.png',
-          linkedIn: 'https://www.linkedin.com/in/joshua-winter-0ab672179/'
-        },
-        {
-          name: 'Bao Phan',
-          role: this.$t('about.bao.role'),
-          bio: this.$t('about.bao.bio'),
-          image: 'Bao.png',
-          linkedIn: 'https://www.linkedin.com/in/baophan0106/'
-        },
-        {
-          name: 'Thuy Hoang',
-          role: this.$t('about.thuy.role'),
-          bio: this.$t('about.thuy.bio'),
-          image: 'Thuy.png',
-          linkedIn: 'https://www.linkedin.com/in/thuy-hoang-936733181/'
-        },
-        {
-          name: 'Nhung Nguyen',
-          role: this.$t('about.nhung.role'),
-          bio: this.$t('about.nhung.bio'),
-          image: 'Nhung.png',
-          linkedIn: 'https://www.linkedin.com/in/thuy-hoang-936733181/'
-        },
-        {
-          name: 'Craig Page',
-          role: this.$t('about.craig.role'),
-          bio: this.$t('about.craig.bio'),
-          image: 'Craig.png',
-          linkedIn: 'https://www.linkedin.com/in/craig-page/'
-        },
-        {
-          name: 'Thuy "Dory" Hua',
-          role: this.$t('about.dory.role'),
-          bio: this.$t('about.dory.bio'),
-          image: 'Thuy2.png',
           linkedIn: ''
         },
         {
-          name: 'An nguyen',
+          name: 'An Nguyen',
           role: this.$t('about.annguyen.role'),
           bio: this.$t('about.annguyen.bio'),
           image: 'annguyen.jpeg',
-          linkedIn: 'https://www.linkedin.com/in/will-macfarlane-b289a333/'
+          linkedIn: ''
+        },
+        {
+          name: 'Hau Le',
+          role: this.$t('about.haule.role'),
+          bio: this.$t('about.haule.bio'),
+          image: 'haule.jpg',
+          linkedIn: ''
         },
         {
           name: 'Minh Nguyet',
           role: this.$t('about.minhnguyet.role'),
           bio: this.$t('about.minhnguyet.bio'),
-          image: 'minhnguyet.jpeg',
+          image: 'minhnguyet.jpg',
           linkedIn: 'https://www.linkedin.com/in/annie-tiny-032723170/'
+        },
+        {
+          name: 'Quang Tran',
+          role: this.$t('about.quangtran.role'),
+          bio: this.$t('about.quangtran.bio'),
+          image: 'quangtran.jpg',
+          linkedIn: ''
+        },
+        {
+          name: 'Hong Quan',
+          role: this.$t('about.hongquan.role'),
+          bio: this.$t('about.hongquan.bio'),
+          image: 'hongquan.jpg',
+          linkedIn: ''
+        },
+        {
+          name: 'Vu Nguyen',
+          role: this.$t('about.vunguyen.role'),
+          bio: this.$t('about.vunguyen.bio'),
+          image: 'vunguyen.jpg',
+          linkedIn: ''
+        },
+        {
+          name: 'Thuy Tran',
+          role: this.$t('about.thuytran.role'),
+          bio: this.$t('about.thuytran.bio'),
+          image: 'thuytran.jpg',
+          linkedIn: ''
         }
 
       ],
       selectedTestimonial: 0,
       testimonials: [
         {
-          body: 'We employed the services of Will and his team at 24h.dev to transform and enhance our company website. After working with us to form a plan, they acted very quickly on providing us with exactly what we needed at the highest of standards and without fuss. Will kept us closely informed as the website progressed and, once completed, ensured we had a thorough understanding of how to manage its very user-friendly administration. We have been deeply impressed by the quality and efficiency of 24h.dev\'s services and would recommend the delightful Will and his savvy team to anyone, anywhere.',
-          client: 'Anthony Stoddart, Director - Beautizon'
+          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget tortor sit amet augue imperdiet placerat. Sed vulputate consequat nulla et sollicitudin. Vivamus vel enim eu justo congue sollicitudin ac sed risus. Curabitur a orci vel ipsum ultricies rhoncus auctor sed sem. Curabitur vel accumsan lacus. ',
+          client: 'Anthony Stoddart, Director - ABC.'
         },
         {
-          body: 'Working with the team at 24h.dev has been a joy. The team translated our brief to reality perfectly, and any feedback we had was actioned quickly and efficiently. Our website has never looked better, and we\'ve seen conversion rates soar, in no small part because of suggestions from Will and the 24h.dev team themselves. Crucially, and having had this difficulty previously, the communication and responsiveness from 24h.dev was second to none, being clear, honest and transparent throughout the project.',
-          client: 'Duncan Kelso, CEO - Himalayan Salt Co.'
+          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget tortor sit amet augue imperdiet placerat. Sed vulputate consequat nulla et sollicitudin. Vivamus vel enim eu justo congue sollicitudin ac sed risus. Curabitur a orci vel ipsum ultricies rhoncus auctor sed sem. Curabitur vel accumsan lacus. ',
+          client: 'Duncan Kelso, CEO - ABC.'
         },
         {
-            body: 'I had my first ever website built by the team at 24h.dev and I couldn\’t be happier. They blended all the design features I liked from various websites into one beautiful page. It\’s look and function is perfect for me, I never thought I would be genuinely happy with a website but they did it, 100% customer satisfaction. The team gave me personalised one-on-one service which was punctual and efficient. Some of the features I needed were not off-the-shelf options and they did a great job of customising the site to my exact needs. They didn\’t just make it how I wanted it, they made it better. I needed a site where it was easy for me to add content or change script and images and they gave me tutoring with clear and easy-to-understand step by step  guidance. One of my biggest needs was to work with a company that I could trust, one that I know is looking out for my best interests. These guys tick every box for me. I have plans for other projects and I have no need to look anywhere else this team is world class, without, out of this world pricing.',
-            client: 'Paul M Dodd, Founder - HeArt of Language'
+          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget tortor sit amet augue imperdiet placerat. Sed vulputate consequat nulla et sollicitudin. Vivamus vel enim eu justo congue sollicitudin ac sed risus. Curabitur a orci vel ipsum ultricies rhoncus auctor sed sem. Curabitur vel accumsan lacus. ',
+          client: 'Paul M Dodd, Founder - ABC.'
         }
       ],
       contactFormResponse: null,
